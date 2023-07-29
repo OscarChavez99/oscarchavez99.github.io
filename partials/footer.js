e4de2c2172f9faa4582a84119fc216a5c49506a9
oscarchavez99 <oscar.chavez4068@alumnos.udg.mx>
2023-07-29 16:35:36 -0600

    responsive bug fixes

--- partials/footer.js
+++ partials/footer.js
@@ -1,15 +1,13 @@
 document.write(`
 <style>
     footer {
-        margin-top: 4vh;
-        margin-bottom: 1vh;
         font-size: 2rem;
     }
 </style>
 
 <link rel="stylesheet" href="../../public/css/footer.css">
-<footer>
-    <div class="text-dark d-flex justify-content-center align-items-center mx-3">
+<footer class="mt-auto bg-dark">
+    <div class="text-light d-flex justify-content-center align-items-center mx-3">
         <p class="container text-center">
             Check my code projects on
             <a href="https://github.com/oscarchavez99" target="_blank">GitHub</a>
