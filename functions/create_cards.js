77eed1e46dede3fcf390c860718b0e37f2351bb4
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-17 23:53:21 -0600

    refactor: skills section

--- functions/create_cards.js
+++ functions/create_cards.js
@@ -1,22 +1,17 @@
-function CreateCards(all_skills, skill_img, rowContainer){
-    
-    for (var i = 0; i<all_skills.length; i++) { 
-        skill_img = "public/img/skills/"; // image route
-        // Trim '#'
-        if (all_skills[i].includes('#')){
-            skill_img += all_skills[i].replace('#', ' sharp');
-        }
-
-        else { 
-            skill_img += all_skills[i]; //skill_image = image route + image name
-        }
+function CreateCards(sanitizedSkills, rowContainer){
+    var skill_img = "";
+    for (var i = 0; i<sanitizedSkills.length; i++) { 
+        // image route
+        skill_img = "public/img/skills/"; 
+        skill_img += sanitizedSkills[i];
+        
         rowContainer.innerHTML += `
         <div class="card mx-2 mb-3" style="width: 15.8rem;">
                 <div class="card-body">
-                    <h5 class="card-title">${all_skills[i]}</h5>
-                    <img src="${skill_img}.png" class="card-img-top img-fluid" alt="${all_skills[i]}">
+                    <h5 class="card-title">${sanitizedSkills[i]}</h5>
+                    <img src="${skill_img}.png" class="card-img-top img-fluid" alt="${sanitizedSkills[i]}">
                     <p class="card-text"></p>
-                    <a href="https://www.google.com/search?q=${encodeURIComponent(all_skills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+                    <a href="https://www.google.com/search?q=${encodeURIComponent(sanitizedSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
                 </div>
             </div>
         `;
