8111bb21b271a798fede032bc235bf8aee9c597e
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-21 12:23:57 -0600

    refactor: display 2 cards per row on mobile version in skills section

--- functions/create-cards.js
+++ functions/create-cards.js
@@ -1,17 +1,18 @@
-function CreateSkillCards(sanitizedSkills, rowContainer, allSkills){
+function CreateSkillCards(sanitizedSkills, rowContainer, allSkills) {
     let skillImg = "";
-    for (var i = 0; i<sanitizedSkills.length; i++) { 
-        // image route
-        skillImg = "public/img/skills/"; 
+    for (let i = 0; i < sanitizedSkills.length; i++) { 
+        skillImg = "public/img/skills/";
         skillImg += sanitizedSkills[i];
-        
+
         rowContainer.innerHTML += `
-        <div class="card mx-2 mb-3" style="width: 14.2rem;">
-                <div class="card-body">
-                    <h5 class="card-title">${allSkills[i]}</h5>
-                    <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${allSkills[i]}">
-                    <p class="card-text"></p>
-                    <a href="https://www.google.com/search?q=${encodeURIComponent(allSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+            <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
+                <div class="card" style="width: 14.2rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${allSkills[i]}</h5>
+                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${allSkills[i]}">
+                        <p class="card-text"></p>
+                        <a href="https://www.google.com/search?q=${encodeURIComponent(allSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+                    </div>
                 </div>
             </div>
         `;
