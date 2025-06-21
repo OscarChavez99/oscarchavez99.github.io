cb29f81d415dd8356455211b9137805ff5381437
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-21 12:10:15 -0600

    chore: rename files to kebab-case for consistency

--- partials/navigation.js
+++ partials/navigation.js
@@ -6,7 +6,7 @@ document.write(`
 <div id="navbar-container">
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark" id="navbar">
     <div class="container">
-        <a class="navbar-brand" href="/">Oscar Chavez</a>
+        <a class="navbar-brand" href="index.html">Oscar Chavez</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
