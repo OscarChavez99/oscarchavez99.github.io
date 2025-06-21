b07bf6d45b779bf93c9ed4c5102ada1d8b41f078
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-21 13:29:07 -0600

    Refactor: Rename variables for clarity and best practices

--- functions/create-cards.js
+++ functions/create-cards.js
@@ -1,17 +1,17 @@
-function CreateSkillCards(sanitizedSkills, rowContainer, allSkills) {
+function CreateSkillCards(sanitizedSkills, skillsHtmlContainer, originalSkills) {
     let skillImg = "";
     for (let i = 0; i < sanitizedSkills.length; i++) { 
         skillImg = "public/img/skills/";
         skillImg += sanitizedSkills[i];
 
-        rowContainer.innerHTML += `
+        skillsHtmlContainer.innerHTML += `
             <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
                 <div class="card" style="width: 14.2rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${allSkills[i]}</h5>
-                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${allSkills[i]}">
+                        <h5 class="card-title">${originalSkills[i]}</h5>
+                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${originalSkills[i]}">
                         <p class="card-text"></p>
-                        <a href="https://www.google.com/search?q=${encodeURIComponent(allSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+                        <a href="https://www.google.com/search?q=${encodeURIComponent(originalSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
                     </div>
                 </div>
             </div>
@@ -19,21 +19,21 @@ function CreateSkillCards(sanitizedSkills, rowContainer, allSkills) {
     }
 }
 
-function CreateIndexCards(fourSanitizedSkills, rowContainer, fourSkills) {
+function CreateIndexCards(fourSanitizedSkills, fourSkillsHtmlContainer, originalFourSkills) {
     let skillImg = "";
     for (let i = 0; i < 4; i++) {
         // image route
         skillImg = "public/img/skills/";
         skillImg += fourSanitizedSkills[i];
         
-        rowContainer.innerHTML += `
+        fourSkillsHtmlContainer.innerHTML += `
             <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
                 <div class="card" style="width: 17rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${fourSkills[i]}</h5>
-                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${fourSkills[i]}">
+                        <h5 class="card-title">${originalFourSkills[i]}</h5>
+                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${originalFourSkills[i]}">
                         <p class="card-text"></p>
-                        <a href="https://www.google.com/search?q=${encodeURIComponent(fourSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+                        <a href="https://www.google.com/search?q=${encodeURIComponent(originalFourSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
                     </div>
                 </div>
             </div>
