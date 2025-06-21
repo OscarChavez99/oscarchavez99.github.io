5392ed48f39eab2c42a0d9a23602a49b665fba46
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-21 12:05:26 -0600

    refactor: display 2 cards per row on mobile version and convert variable names to camelCase

--- functions/create_cards.js
+++ functions/create_cards.js
@@ -1,15 +1,15 @@
 function CreateSkillCards(sanitizedSkills, rowContainer, allSkills){
-    let skill_img = "";
+    let skillImg = "";
     for (var i = 0; i<sanitizedSkills.length; i++) { 
         // image route
-        skill_img = "public/img/skills/"; 
-        skill_img += sanitizedSkills[i];
+        skillImg = "public/img/skills/"; 
+        skillImg += sanitizedSkills[i];
         
         rowContainer.innerHTML += `
         <div class="card mx-2 mb-3" style="width: 14.2rem;">
                 <div class="card-body">
                     <h5 class="card-title">${allSkills[i]}</h5>
-                    <img src="${skill_img}.png" class="card-img-top img-fluid" alt="${skill_img[i]}">
+                    <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${allSkills[i]}">
                     <p class="card-text"></p>
                     <a href="https://www.google.com/search?q=${encodeURIComponent(allSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
                 </div>
@@ -18,18 +18,22 @@ function CreateSkillCards(sanitizedSkills, rowContainer, allSkills){
     }
 }
 
-function CreateIndexCards(fourSanitizedSkills, rowContainer, four_skills) {
-    let skill_img = "";
+function CreateIndexCards(fourSanitizedSkills, rowContainer, fourSkills) {
+    let skillImg = "";
     for (let i = 0; i < 4; i++) {
-        skill_img = "public/img/skills/";
-        skill_img += fourSanitizedSkills[i];
+        // image route
+        skillImg = "public/img/skills/";
+        skillImg += fourSanitizedSkills[i];
+        
         rowContainer.innerHTML += `
-            <div class="card mx-3 mb-3" style="width: 17rem;">
-                <div class="card-body">
-                    <h5 class="card-title">${four_skills[i]}</h5>
-                    <img src="${skill_img}.png" class="card-img-top img-fluid" alt="${skill_img[i]}">
-                    <p class="card-text"></p>
-                    <a href="https://www.google.com/search?q=${encodeURIComponent(four_skills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+            <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
+                <div class="card" style="width: 17rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${fourSkills[i]}</h5>
+                        <img src="${skillImg}.png" class="card-img-top img-fluid" alt="${fourSkills[i]}">
+                        <p class="card-text"></p>
+                        <a href="https://www.google.com/search?q=${encodeURIComponent(fourSkills[i])}" target="_blank" class="btn btn-primary">Learn More</a>
+                    </div>
                 </div>
             </div>
         `;
