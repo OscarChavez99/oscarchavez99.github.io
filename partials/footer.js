ef9c70588adc2e02ee9d9dac0e4fbb220f84259a
oscarchavez99 <oscar.chavez.gomez.99@gmail.com>
2025-06-21 13:07:25 -0600

    refactor: replace document.write with dynamic footer injection

--- partials/footer.js
+++ partials/footer.js
@@ -1,17 +1,16 @@
-document.write(`
-<style>
-    footer {
-        font-size: 2rem;
-        flex-shrink: 0;
-    }
-</style>
-
-<footer class="mt-auto bg-dark">
-    <div class="text-light d-flex justify-content-center align-items-center mx-3">
-        <p class="container text-center">
-            Check my code projects on
-            <a href="https://github.com/oscarchavez99" target="_blank">GitHub</a>
-        </p>
-    </div>
-</footer>
-`);
+document.getElementById("footer").innerHTML = `
+    <style>
+        footer {
+            font-size: 2rem;
+            flex-shrink: 0;
+        }
+    </style>
+    <footer class="mt-auto bg-dark">
+        <div class="text-light d-flex justify-content-center align-items-center mx-3">
+            <p class="container text-center">
+                Check my code projects on
+                <a href="https://github.com/oscarchavez99" target="_blank">GitHub</a>
+            </p>
+        </div>
+    </footer>
+`;
